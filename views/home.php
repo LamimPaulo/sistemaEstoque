dd5d28eb1b28b77761a964b1bf78da0e82a14aee
Paulo Lamim <Robertolamim@gmail.com>
2019-07-01 14:50:52 -0300

    add e edit de items no sistema

--- views/home.php
+++ views/home.php
@@ -1,1 +1,34 @@
-Home
+<a href="<?php echo BASE_URL; ?>home/add">Adicionar Produto</a> <br><br>
+
+<fieldset>
+    <form method="get">
+        <input type="text" id="busca" name="busca" value="<?php echo (!empty($_GET['busca']))?$_GET['busca']:''; ?>" placeholder="Buscar por Codigo de barras ou Nome do produto" style="width:100%;height:35px;font-size:18px;">
+    </form>
+</fieldset>
+<br><br>
+
+<table border="1" width="100%">
+    <tr>
+        <th>Cód.</th>
+        <th>Nome</th>
+        <th>Preço Unit</th>
+        <th>Qtd.</th>
+        <th>Açoes</th>
+    </tr>
+    <?php foreach ($list as $item): ?>
+        <tr>
+            <td><?php echo $item['cod']; ?></td>
+            <td><?php echo $item['name']; ?></td>
+            <td>R$ <?php echo number_format($item['price'], 2, ',', '.'); ?></td>
+            <td><?php echo $item['quantity']; ?></td>
+            <td>
+                <a href="<?php echo BASE_URL; ?>home/edit/<?php echo $item['id'];?>">Editar</a>
+            </td>
+        </tr>
+    <?php endforeach; ?>
+
+</table>
+
+<script type="text/javascript">
+document.getElementById("busca").focus();
+</script>
